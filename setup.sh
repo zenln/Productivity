db9ea403778ec79df70d15d028d48ab762d1cdf5
Kansakar, Alen R [ITSYS] <kansakar@iastate.edu>
2018-06-27 11:03:46 -0500

    Fixing typo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 # All file with dotNAME will be linked to home
 for i in $(ls dot*);do echo $i; ln -s $PWD/$i $HOME/.$(echo $i | sed 's/dot//g');done
-if command -v gits; then
+if command -v git; then
   git clone https://github.com/nojhan/liquidprompt.git
 else
   echo "############################"
